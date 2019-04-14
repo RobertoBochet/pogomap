bc46dfd256a9cdad3b2332bf641b4eadf1da99b9
Roberto Bochet <robertobochet@gmail.com>
2019-04-15 01:04:38 +0200

    Fixed unverified entity 500 error

--- app/pogomap/entities/mkentity.py
+++ app/pogomap/entities/mkentity.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+from .Entity import Entity
 from .Portal import Portal
 from .Pokestop import Pokestop
 from .Gym import Gym
 
 def mkentity(data):
+	if not "type" in data:
+		return Entity(data)
 	if data["type"] == "portal":
 		return Portal(data)
 	elif data["type"] == "pokestop":
