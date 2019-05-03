aaef3b5f147df1b3c84f1796392fd254cd29849d
Roberto Bochet <robertobochet@gmail.com>
2019-05-03 02:58:53 +0200

    Used gunicorn

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,6 @@ RUN apk add gcc python3-dev musl-dev postgresql-dev
 RUN python -m pip install --upgrade pip
 RUN pip install -r ./requirements.txt
 
-ENTRYPOINT ./app.py
+EXPOSE 5775
+
+ENTRYPOINT gunicorn -b 0.0.0.0:5775 app:app
--- app/app.py
+++ app/app.py
@@ -9,41 +9,39 @@ import logging
 import signal
 import sys
 
-reggie = Reggie()
 app = Flask(__name__)
-reggie.init_app(app)
+Reggie(app)
 
 
 @app.route("/get_entities/<regex('(gyms(_eligible)?)|(pokestop(_eligible)?)|((not_)?in_pogo)|unverified'):type>/")
 def get_entities(type):
-	respose = pogomap.query(type)
-	return respose.json
+    response = pogomap.query(type)
+    return response.json
+
 
 @app.route("/<regex('[0-9a-zA-Z]{32}'):key>/")
 def map_key(key):
-	return render_template("index.html",key=key)
+    return render_template("index.html", key=key)
 
-@app.route('/')
+
+@app.route("/")
 def map():
-	return render_template("index.html")
+    return render_template("index.html")
 
-@app.route('/favicon.ico')
+
+@app.route("/favicon.ico")
 def favicon():
     return redirect("/static/favicon/favicon.ico", code=301)
 
 
-def signal_handler(sig, frame):
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-if __name__ == '__main__':
-	logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] %(message)s")
 
-	pogomap = PogoMap(
-		db_host = os.environ["DB_HOST"],
-		db_user = os.environ["DB_USER"],
-		db_pass = os.environ["DB_PASS"],
-		db_name = os.environ["DB_NAME"]
-	)
+pogomap = PogoMap(
+    db_host=os.environ["DB_HOST"],
+    db_user=os.environ["DB_USER"],
+    db_pass=os.environ["DB_PASS"],
+    db_name=os.environ["DB_NAME"]
+)
 
-	app.run(debug=False,host='0.0.0.0',port=5775)
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=5775)
