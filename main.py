c1796fa6bd677e305b8c779f5f8e7709ea0c5dd8
Roberto Bochet <robertobochet@gmail.com>
2019-07-29 00:02:44 +0200

    Minor bug fixed

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import argparse
 import logging
 
-import log
 from webapp import WebApp
 
 if __name__ == "__main__":
@@ -16,11 +15,11 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--pg-db-name", dest="pg_db_name", help="postgres database name")
     parser.add_argument("-f", "--flask-port", dest="flask_port", help="flask listening port")
 
-    parser.add_argument("-v", dest="verbosity_level", action="count", default=0,
+    parser.add_argument("-v", dest="log_level", action="count", default=0,
                         help="number of -v specifics level of verbosity")
-    parser.add_argument("--info", dest="verbosity_level", action="store_const", const=2,
+    parser.add_argument("--info", dest="log_level", action="store_const", const=2,
                         help="equal to -vv")
-    parser.add_argument("--debug", dest="verbosity_level", action="store_const", const=3,
+    parser.add_argument("--debug", dest="log_level", action="store_const", const=3,
                         help="equal to -vvv")
 
     # Parses args
@@ -36,11 +35,8 @@ if __name__ == "__main__":
         3: logging.DEBUG
     }
 
-    # Find verbosity level
-    try:
-        args["verbosity_level"] = VERBOSITY_LEVELS[args.pop("verbosity_level")]
-    except KeyError:
-        pass
+    # Compute verbosity level
+    args["log_level"] = VERBOSITY_LEVELS[args["log_level"]] if args["log_level"] < 4 else VERBOSITY_LEVELS[3]
 
     # Find flask port
     try:
@@ -52,4 +48,4 @@ if __name__ == "__main__":
     app = WebApp(**args)
 
     # Run the web app
-    app.run(port=flask_port)
+    app.run(debug=True, port=flask_port)
